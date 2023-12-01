7b99e8e6bc44c01c57474f43125e365b7f389c2a
Jason Liu <neopql@gmail.com>
2023-12-01 12:06:16 -0800

    altered for lab report 5

--- grade.sh
+++ grade.sh
@@ -1,27 +1,3 @@
-# ListExamples.java implementation guide
-
-# filter()
-# Returns a new list that has all the elements of the input list for which
-# the StringChecker returns true, and not the elements that return false, in
-# the same order they appeared in the input list;
-
-# merge()
-# Takes two sorted list of strings (so "a" appears before "b" and so on),
-# and return a new list that has all the strings in both list in sorted order.
-
-
-# plan of action
-#   1. check a file called ListExamples.java exists and is in the right directory ✓
-#   2. check in ListExamples.java that a class called ListExamples exists, maybe with grep "class ListExamples" ✓
-#   3. check if filter() and merge() files exists with the correct signatures, with grep ✓
-#   4. run javac -cp $CPATH *.java
-#          java -cp $CPATH TestListExamples
-#   5. profit
-
-# Questions
-# 
-
-
 CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 score=0
@@ -69,8 +45,8 @@ else
 fi
 
 # copies needed files into grading area
-cp student-submission/ListExamples.java TestListExamples.java grading-area
-cp -r lib grading-area
+# cp student-submission/ListExamples.java TestListExamples.java grading-area
+# cp -r lib grading-area
 
 cd grading-area
 pwd
@@ -86,9 +62,3 @@ then
 fi
 
 echo "Score = $score out of 1"
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
--- grading-area/ListExamples.java
+++ grading-area/ListExamples.java
@@ -8,10 +8,13 @@ class ListExamples {
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
-  static List<String> filter(List<String> a, StringChecker b) {
+  static List<String> filter(List<String> a, StringChecker b) 
+  {
     List<String> result = new ArrayList<>();
-    for(String s: a) {
-      if(b.checkString(s)) {
+    for(String s: a) 
+    {
+      if(b.checkString(s)) 
+      {
         result.add(s);
       }
     }
@@ -21,24 +24,30 @@ class ListExamples {
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
-  static List<String> merge(List<String> x, List<String> y) {
+  static List<String> merge(List<String> x, List<String> y) 
+  {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
-    while(index1 < x.size() && index2 < y.size()) {
-      if(x.get(index1).compareTo(y.get(index2)) < 0) {
+    while(index1 < x.size() && index2 < y.size()) 
+    {
+      if(x.get(index1).compareTo(y.get(index2)) < 0) 
+      {
         result.add(x.get(index1));
         index1 += 1;
       }
-      else {
+      else 
+      {
         result.add(y.get(index2));
         index2 += 1;
       }
     }
-    while(index1 < x.size()) {
+    while(index1 < x.size()) 
+    {
       result.add(x.get(index1));
       index1 += 1;
     }
-    while(index2 < y.size()) {
+    while(index2 < y.size()) 
+    {
       result.add(y.get(index2));
       index2 += 1;
     }
